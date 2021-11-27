8de6bb03183393ba638d7e38b6b869901c03f808
me-marchi <me.marchi@outlook.com>
2021-11-27 16:04:50 -0300

    atualização front

--- src/app.js
+++ src/app.js
@@ -6,11 +6,15 @@ const index = require("./routes/index");
 
 app.use(express.json());
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Origin", "Origin, X-Request-With, Content-Type, Accept");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
-});
+  });
+  
 //O asterico significa "tudo"
 //Os "Access-Control-Allow-Origin" significam que a aplicação pode receber dados de qualquer origem, de modo a não haver problemas com o front
 //O next indica que ele ele pode seguir fora da função, impedindo que a aplicação fique travada nestas configurações header
